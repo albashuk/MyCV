df56e0a4f5ef8b3136eac1a86be1a925c3b6e921
Oleksii <al.bashuk@gmail.com>
2019-05-04 22:28:26 +0300

    CV

---
+++ MyCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Oleksii Bashuk on 5/1/19.
+//  Copyright © 2019 Oleksii Bashuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
